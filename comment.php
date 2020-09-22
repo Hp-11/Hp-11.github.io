d8dc0b69665caa5b3a36dc73d8034cfba608e546
Hp-11 <64689840+Hp-11@users.noreply.github.com>
2020-09-22 16:59:00 +0530

    space-black

---
+++ comment.php
@@ -0,0 +1,25 @@
+<?php 
+// $name = $_POST["name"];
+// $email = $_POST["email"];
+// $comment = $_POST["comment"];
+
+// if(!empty($name)||!empty($email)||!empty($comment)){
+//     $host = "localhost";
+//     $dbUsername ="HP-12";
+//     $dbPassword = "basketball";
+//     $dbname ="portfolio_database-comments";
+
+//     //creating connection
+//     $conn = new mysqli($host, $dbUsername,$dbPassword,$dbname);
+
+//     if(mysqli_connect_error()){
+//         die('Connect Error('.mysqli_connect_error().')'. mysqli_connect_error());
+//      } else{
+//          $SELECT = "SELECT email FROM register WHERE email=? ";
+//          $INSERT ="INSERT Into register (name,email,,comment) values(?,?,,?)";
+
+//      }
+// }
+
+
+?>
