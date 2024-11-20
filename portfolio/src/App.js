16cf7736d2e0057606db89c28c4b501eb0cd8dd1
Harsh Pundir <harshpundir0103@gmail.com>
2024-11-20 09:24:43 +0530

    making a new version

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+     <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+     </Router>
+  );
+}
+
+export default App;
